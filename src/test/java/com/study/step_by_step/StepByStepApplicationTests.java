8c4c2383d1403e0e7c724499a5e45fb07e8932c2
Jeongyounghyeon <snorlax1106@naver.com>
2024-09-23 17:06:36 +0900

    chore: gradle project 생성

---
+++ src/test/java/com/study/step_by_step/StepByStepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.step_by_step;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepByStepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
